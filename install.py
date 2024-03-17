abdc9b3ec8161589ddf30818e1ff3af376fedc34
soukkouyoucef <soukkouyoucef@gmail.com>
2024-03-18 00:49:31 +0100

    implementing virtual environments using venv

--- install.py
+++ install.py
@@ -1,8 +1,9 @@
 import os 
 
-command = "pip install"
+virt_env = "python3.11 -m venv encyenv; source encyenv/bin/activate"
+os.system(virt_env)
+command = "pip install "
 dependencies = ["PySide2","bs4","requests","PyQt5"]
-
 for dependency in dependencies: 
     install = "{COMMAND} {DEPENDENCY}".format(COMMAND=command, DEPENDENCY=dependency)
     os.system(install)
